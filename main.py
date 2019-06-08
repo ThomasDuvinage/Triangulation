2a5ccccf6cf943712a0aecd65392d9b39cedfb48
totordudu <43036801+totordudu@users.noreply.github.com>
2019-06-08 15:35:28 +0200

    cercle

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(7,7))
 plt.axis([-10,210,-10,210])
 
-points_number = 13
+points_number = 20
 final_triangles = []
 
 select_point = generate_points(points_number,200,200,'random')
@@ -31,26 +31,27 @@ for row in range(triangle.number_of_triangle):
     
     circle_size = cercle_parameters(select_point.points[triangle.triangles[row][0]][0],select_point.points[triangle.triangles[row][0]][1],select_point.points[triangle.triangles[row][1]][0],select_point.points[triangle.triangles[row][1]][1],select_point.points[triangle.triangles[row][2]][0],select_point.points[triangle.triangles[row][2]][1])
     
-    print("x  = ",circle_size.X0)
-    print("y  = ",circle_size.Y0)
-    print("R  = ",circle_size.Rc)
-
-
-    circle = create_circle(circle_size.X0,circle_size.Y0,circle_size.Rc)
-    #show_circle(circle)
-
     n = 0
     info = 0
-    while n < select_point.number_point:
-        distance = math.sqrt((select_point.points[n][0] - circle_size.X0)**2 + (select_point.points[n][1] - circle_size.Y0)**2)
 
-        if(round(distance,3) < round(circle_size.Rc,3)):
-            info += 1
-            print("distance = ",distance)  
-        
-        n+=1  
+    print("indice = ",circle_size.indice_check)
+    if(circle_size.indice_check == 1):
+        print("x  = ",circle_size.X0)
+        print("y  = ",circle_size.Y0)
+        print("R  = ",circle_size.Rc)
 
-    if(info == 0):
+        while n < select_point.number_point:
+            distance = math.sqrt((select_point.points[n][0] - circle_size.X0)**2 + (select_point.points[n][1] - circle_size.Y0)**2)
+
+            if(round(distance,3) < round(circle_size.Rc,3)):
+                info += 1
+                #print("distance = ",distance)  
+            
+            n+=1  
+
+        if(info == 0):
+            circle = create_circle(circle_size.X0,circle_size.Y0,circle_size.Rc)
+            show_circle(circle)
             final_triangles.append(triangle.triangles[row])
         
 display_all_triangles(final_triangles,select_point)
--- scripts/cercle.py
+++ scripts/cercle.py
@@ -5,22 +5,24 @@ import matplotlib.pyplot as plt
 class cercle_parameters:
     def __init__(self,x1,y1,x2,y2,x3,y3):
         
+        self.indice_check = 0
+        if((y3 - y2) != 0 and (y2-y1) != 0 and (2*(((x3 - x2) / (y3 - y2)) - ((x2 - x1)) != 0))):
+            #permet de connaitre la position en x,y et le rayon du centre cercle qui passe par les 3 points
+            calc1 = (pow(x3,2) - pow(x2,2)) / (y3 - y2)
+            calc2 = (pow(x2,2) - pow(x1,2)) / (y2 - y1)
+            calc3 = (x3 - x2) / (y3 - y2)
+            calc4 = 2*(calc3 - ((x2 - x1) / (y2 - y1)))
 
-        #permet de connaitre la position en x,y et le rayon du centre cercle qui passe par les 3 points
-        calc1 = (pow(x3,2) - pow(x2,2)) / (y3 - y2)
-        calc2 = (pow(x2,2) - pow(x1,2)) / (y2 - y1)
-        calc3 = (x3 - x2) / (y3 - y2)
-        calc4 = 2*(calc3 - ((x2 - x1) / (y2 - y1)))
+            x = (calc1 - calc2 + y3 - y1) / calc4
 
-        x = (calc1 - calc2 + y3 - y1) / calc4
+            y = 0.5 * (((-2 * x * (x2 - x1)) / (y2 - y1)) + ((pow(x2,2) - pow(x1,2)) / (y2 - y1)) + y2 + y1)
 
-        y = 0.5 * (((-2 * x * (x2 - x1)) / (y2 - y1)) + ((pow(x2,2) - pow(x1,2)) / (y2 - y1)) + y2 + y1)
+            R = sqrt(pow((x1 - x),2) + pow((y1 - y),2))
 
-        R = sqrt(pow((x1 - x),2) + pow((y1 - y),2))
-
-        self.X0 = x
-        self.Y0 = y
-        self.Rc = R
+            self.X0 = x
+            self.Y0 = y
+            self.Rc = R
+            self.indice_check = 1
 
 
 def create_circle(x,y,R):
